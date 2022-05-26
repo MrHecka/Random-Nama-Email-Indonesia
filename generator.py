eabefc41516a0344f9451345e727a3df7d201217
MrHecka <muhammadmamad41@gmail.com>
2022-05-26 20:15:59 +0700

    gabut ngab :v

--- generator.py
+++ generator.py
@@ -1,6 +1,6 @@
 import requests as r
 from lxml import html
-import random, os
+import random, os, clipboard
 
 
 a = r.get("http://ninjaname.horseridersupply.com/indonesian_name.php")
@@ -14,6 +14,7 @@ os.system('cls')
 print("========RANDOMIZE NAMA + EMAIL BY HECKAYO========")
 print("")
 
+
 for x in nama:
     ran = random.randint(0, len(nama))
     filter = nama[ran]
@@ -22,8 +23,10 @@ for x in nama:
     namaemail = ''.join(c.lower() for c in nama if not c.isspace())
     print("Nama : " + nama)
     print(f"Email : {namaemail}{emailnum}@gmail.com")
+    clipboard.copy(f"{namaemail}{emailnum}@gmail.com")
+    print("")
+    print("Email Berhasil Di Copy!")
     break
-
 
 
 
